0697722acba598c20bd4e0d48710e41781ecac42
Pradeep_Kumar <pradeepvijay170@gmail.com>
2025-11-08 01:15:42 +0530

    Update publicAxios.js

--- frontend/src/utils/publicAxios.js
+++ frontend/src/utils/publicAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const publicAxios = axios.create({
-  baseURL: 'http://localhost:5000/api', // or your live backend URL
+  baseURL: 'https://cctvshoppee.onrender.com/api', // or your live backend URL
   headers: { 'Content-Type': 'application/json' }
 });
 
