d61038e4309ae43ee15e3dbea824cfbb132659d0
Pradeep_Kumar <pradeepvijay170@gmail.com>
2025-11-08 01:15:17 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // your backend API
+  baseURL: 'https://cctvshoppee.onrender.com/api', // your backend API
   headers: { 'Content-Type': 'application/json' },
 });
 
