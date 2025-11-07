39257e079ec146a44a0e4a7174eae5f34ff4b2b5
Pradeep_Kumar <pradeepvijay170@gmail.com>
2025-11-08 01:15:00 +0530

    Update api.js

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/products';
+const API_URL = 'https://cctvshoppee.onrender.com/api/products';
 
 export const getProducts = (params) => axios.get(API_URL, { params });
 export const getProduct = (id) => axios.get(`${API_URL}/${id}`);
