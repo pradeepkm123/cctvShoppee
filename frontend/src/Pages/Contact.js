d26bb11de4147251d780e835bec108a85afc1c48
Pradeep_Kumar <pradeepvijay170@gmail.com>
2025-11-14 12:45:59 +0530

    Update Contact.js

--- frontend/src/Pages/Contact.js
+++ frontend/src/Pages/Contact.js
@@ -147,7 +147,7 @@ function About() {
                            </span>
                         </div>
                         <div class="tp-map-iframe">
-                           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830894612!2d-74.11976383964465!3d40.69766374865766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1678114595329!5m2!1sen!2sbd"></iframe>
+                                   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6604.864239347136!2d80.1490307!3d13.0356358!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52615d5e910e59%3A0x9ea7a355f133ac04!2sCCTV%20SHOPPEE%20-%20PORUR!5e1!3m2!1sen!2sin!4v1763104372029!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                      </div>
                   </div>
